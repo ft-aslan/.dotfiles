e848c64b5f2f7fb8877361867ef617e7622f92c2
ft-aslan <fthaslncn@gmail.com>
2024-07-02 19:08:00 +0300

    switched to new multi cursor plugin

--- .config/nvim/lua/custom/plugins/util.lua
+++ .config/nvim/lua/custom/plugins/util.lua
@@ -118,11 +118,11 @@ return {
         init = function()
           -- Require providers
           require 'hover.providers.lsp'
-          -- require('hover.providers.gh')
-          -- require('hover.providers.gh_user')
-          -- require('hover.providers.jira')
-          -- require('hover.providers.man')
-          -- require('hover.providers.dictionary')
+          require 'hover.providers.gh'
+          require 'hover.providers.gh_user'
+          require 'hover.providers.dap'
+          require 'hover.providers.diagnostic'
+          require 'hover.providers.dictionary'
         end,
         preview_opts = {
           border = 'single',
